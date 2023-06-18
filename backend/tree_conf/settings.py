c2994749b6fa709cbe27c01309fb3f3d3123a35b
Rakhmatullo929 <rakhmatullo@192.168.1.167>
2023-06-18 14:44:23 +0500

    add admin panel jazzmin

--- backend/tree_conf/settings.py
+++ backend/tree_conf/settings.py
@@ -41,6 +41,7 @@ sys.path.append(os.path.join(BASE_DIR, 'apps'))
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
