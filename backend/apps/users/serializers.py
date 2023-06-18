ec4ef354ba0d2e270e9ee378ec8d33958130ab1d
WebDeveloperM <webdevmaruf1514@gmail.com>
2023-06-18 16:41:49 +0500

    craeted count field in oreder model

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,23 +1,51 @@
 from rest_framework import serializers
-from .models import User
-<<<<<<< HEAD
-import random
-=======
->>>>>>> 43624627e8bef71fda02dde150c39954318c9194
+from users.models import User, Token
+from users.models import SmsCode
+from rest_framework.exceptions import ParseError
+from datetime import datetime
+from rest_framework.response import Response
+
+
+# import random
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'phone', "code")
+        fields = ('id', 'phone')
 
-    def create(self, validated_data):
-        phone = validated_data.pop('phone', None)
-        code = str(random.randint(0, 2814749767106558635674786786384))[:4]
+    # def create(self, validated_data):
+    #     phone = validated_data.get('phone', None)
+    #
+    #     print(phone in int, "6"*88)
+    #     if len(phone) == 12:
+    #
+    #         instance = self.Meta.model(phone=phone, code=code, **validated_data)
+    #         instance.save()
+    #         return instance
+    #     raise ValueError("The phone number must be at least 12 characters long")
 
-        if len(phone) == 12:
-            instance = self.Meta.model(phone=phone, code=code, **validated_data)
-            instance.save()
-            return instance
-        raise ValueError("The phone number must be at least 12 characters long")
-
+   #
+   # phone = validated_data.data.get('phone')
+   #      dispatch_id = validated_data.data.get('dispatch_id')
+   #      code = validated_data.data.get('verification_code')
+   #      sms_code = SmsCode.objects.filter(dispatch_id=dispatch_id).first()
+   #
+   #      if not sms_code or sms_code.code != code:
+   #          raise ParseError('Verification code incorrect. Try again.', 400)
+   #
+   #      user, created = User.objects.update_or_create(phone=phone, email=phone, username=phone)
+   #      user.last_login = datetime.now()
+   #      user.save()
+   #
+   #      if created:
+   #          token = Token.objects.create(user=user)
+   #      else:
+   #          t = Token.objects.filter(user=user).first()
+   #          t.key = tokens.generate()
+   #          t.expires_at = expires_default()
+   #          t.save()
+   #          token = t
+   #      SmsCode.objects.exclude(id=1).delete()
+   #      user_serializer = UserSerializer(instance=user).data
+   #      return Response({"user": user_serializer, "token": token.key}, 201)
