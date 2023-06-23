119a82acbdba42985fe61e641b2637ccb159f1cd
Mirzohid Kenjayev <boburovich0508@gmail.com>
2023-06-23 22:03:34 +0500

    change serializers

--- backend/apps/main/urls.py
+++ backend/apps/main/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from main.views import PlantCreateListView, OrderCreateListView, AllJobsAPIView
+from main.views import PlantCreateListView, OrderCreateListView, AllJobsAPIView, OrderListView
 
 urlpatterns = [
-<<<<<<< HEAD
+
     path('plant/',PlantCreateListView.as_view(),name='plant'),
     path('order/',OrderListView.as_view(),name='order'),
  
-=======
+
     path('plant/', PlantCreateListView.as_view(), name='plant'),
     path('order/', OrderCreateListView.as_view(), name='order'),
     path('all-jobs/', AllJobsAPIView.as_view(), name='all-jobs'),
 
->>>>>>> 6e9e5b58a17433944177bf3ffd2f36774a03db3f
+
 ]
