4f9c3cef47f25ac5e55e3ce38d1f94671df9cf0c
WebDeveloperM <webdevmaruf1514@gmail.com>
2023-06-25 16:15:02 +0500

    order done api view

--- backend/apps/main/serializers.py
+++ backend/apps/main/serializers.py
@@ -26,7 +26,7 @@ class OrderSerializer(serializers.ModelSerializer):
 class OrderDoneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ('id', 'status', 'location')
+        fields = ('id', 'location' )
 
     def update(self, instance, data):
         user = self.context['user']
@@ -45,8 +45,7 @@ class OrderDoneSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         ret = super().to_representation(instance)
-        ret["count"] = instance.count
-        ret["amount"] = instance.count * 5
+        ret["message"] = "This plant have done"
         return ret
 
 
