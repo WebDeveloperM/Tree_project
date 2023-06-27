8e4a6b55ad14ebc3ea4e9091b897423ec9bcf416
rakhmatov1ch <nurmuxammadrahmatov@gmail.com>
2023-06-27 21:06:06 +0500

    chart

--- frontend/screens/Home.js
+++ frontend/screens/Home.js
@@ -4,6 +4,8 @@ import {useFocusEffect, useNavigation, useRoute} from "@react-navigation/native"
 import InvestorFooter from "../components/InvestorFooter";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+import Chart from "../components/Chart";
+import {INVESTOR_ORDERS} from "./utils/urls";
 
 
 export default function Home() {
@@ -24,7 +26,7 @@ export default function Home() {
                 Authorization: `Token ${token}`,
                 'Content-Type': 'application/json',
             };
-            const response = await axios.get('http://127.0.0.1:8000/api/v1/finance/investor-orders/', {headers})
+            const response = await axios.get(INVESTOR_ORDERS, {headers})
             // console.log(response.data);
             setOrders(response.data)
         } catch (error) {
@@ -50,26 +52,31 @@ export default function Home() {
                     className='absolute top-4 left-4 border border-[#1B772E] rounded-xl py-2 px-4 z-10 bg-white'>
                     <Text className='text-[16px] text-[#1B772E] font-semibold'>Log out</Text>
                 </Pressable>
-                <View className='h-56 w-full bg-gray-400 items-center justify-center'>
-                    <Text>view for chart</Text>
+                <View className='mt-8 w-full items-center justify-center'>
+                    <Chart />
                 </View>
                 <Text className='text-[30px] font-semibold my-12'>Wellcome</Text>
                 <View className='items-center w-full'>
-                    {orders ? orders.map(order => (
-                        <View key={order.id}
-                              className='w-[80%] mb-4 h-24 flex-row items-center bg-[#D6F0DB]/30 p-2 border border-[#1B772E] rounded-2xl'>
-                            <View className='border-2 border-[#1B772E] rounded-2xl p-2 bg-white mr-3'>
-                                <Image className='h-14 w-14' source={require('../assets/tree.png')}/>
-                            </View>
-                            <View className=''>
-                                <Text className='text-[14px] text-[#0AC16D] font-semibold'>The first tree</Text>
-                                <Text className='text-[20px] my-1'>{order.count} tree</Text>
-                                <Text className='text-[12px]'>Lorem ipsum</Text>
-                            </View>
-                            <View className='ml-auto mr-2'>
-                                <Image className='w-12 h-14' source={require('../assets/location-green.png')}/>
-                            </View>
-                        </View>)) : <Text>You have not invested yet :(</Text>}
+                    {orders.length ? orders.map(order => (
+                            <View key={order.id}
+                                  className='w-[80%] mb-4 h-24 flex-row items-center bg-[#D6F0DB]/30 p-2 border border-[#1B772E] rounded-2xl'>
+                                <View className='border-2 border-[#1B772E] rounded-2xl p-2 bg-white mr-3'>
+                                    <Image className='h-14 w-14' source={require('../assets/tree.png')}/>
+                                </View>
+                                <View className=''>
+                                    <Text className='text-[14px] text-[#0AC16D] font-semibold'>The first tree</Text>
+                                    <Text className='text-[20px] my-1'>{order.count} tree</Text>
+                                    <Text className='text-[12px]'>Lorem ipsum</Text>
+                                </View>
+                                <View className='ml-auto mr-2'>
+                                    <Image className='w-12 h-14' source={require('../assets/location-green.png')}/>
+                                </View>
+                            </View>))
+                        :
+                        <View className='w-[80%] bg-[#D6F0DB] h-10 rounded-xl items-center justify-center'>
+                            <Text>You have not invested yet</Text>
+                        </View>
+                    }
                 </View>
             </View>
         </ScrollView>
--- frontend/screens/utils/urls.js
+++ frontend/screens/utils/urls.js
@@ -7,11 +7,10 @@ export const LOGIN = `${domain}/api/v1/users/login/`
 export const PAYMENTS = 'http://127.0.0.1:8000/api/v1/finance/payments/'
 export const ADD_CARD = 'http://127.0.0.1:8000/api/v1/finance/cards/'
 export const GET_CARD = 'http://127.0.0.1:8000/api/v1/finance/cardslist/'
-
 export const GET_JOBS = 'http://127.0.0.1:8000/api/v1/main/order/'
 export const GET_ORDERS_DATA = 'http://127.0.0.1:8000/api/v1/main/full-order-data/'
 export const GET_MY_WORK = 'http://127.0.0.1:8000/api/v1/main/farmer-order/'
-
 export const ORDER_CHANGE = 'http://127.0.0.1:8000/api/v1/main/order-change/'
-
 export const LAST_ORDERS = 'http://127.0.0.1:8000/api/v1/main/last-orders/'
+export const INVESTOR_ORDERS = 'http://127.0.0.1:8000/api/v1/finance/investor-orders/'
+export const MAIN_PLANTS = 'http://127.0.0.1:8000/api/v1/main/plant/'
